4627fc70b91d028135022575413851ca5c764d99
Akshaj Nair <34983749+Akshajnair@users.noreply.github.com>
2021-05-27 12:41:09 +0530

    Update Game.js

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -192,34 +192,34 @@ export class Game extends Component {
   render() {
     return (
       <div>
-        <section class="u-clearfix u-grey-5 u-section-3" id="sec-bc23">
-          <div class="u-clearfix u-sheet u-sheet-1">
-            <h1 class="u-custom-font u-font-lobster u-text u-text-default u-text-palette-4-base u-title u-text-1">
+        <section className="u-clearfix u-grey-5 u-section-3" id="sec-bc23">
+          <div className="u-clearfix u-sheet u-sheet-1">
+            <h1 className="u-custom-font u-font-lobster u-text u-text-default u-text-palette-4-base u-title u-text-1">
               Round {this.state.round}
             </h1>
-            <h1 class="u-custom-font u-font-lobster u-text u-text-default u-text-grey-70 u-text-2">
+            <h1 className="u-custom-font u-font-lobster u-text u-text-default u-text-grey-70 u-text-2">
               Machine
             </h1>
-            <h1 class="u-align-right-md u-align-right-sm u-align-right-xs u-custom-font u-font-lobster u-text u-text-default u-text-grey-70 u-text-3">
+            <h1 className="u-align-right-md u-align-right-sm u-align-right-xs u-custom-font u-font-lobster u-text u-text-default u-text-grey-70 u-text-3">
               You
             </h1>
-            <h1 class="u-custom-font u-font-lobster u-text u-text-default u-text-grey-70 u-text-4">
+            <h1 className="u-custom-font u-font-lobster u-text u-text-default u-text-grey-70 u-text-4">
               Tie
             </h1>
-            <h2 class="u-align-left-md u-align-left-sm u-align-left-xs u-custom-font u-font-lobster u-text u-text-default u-text-grey-50 u-text-5">
+            <h2 className="u-align-left-md u-align-left-sm u-align-left-xs u-custom-font u-font-lobster u-text u-text-default u-text-grey-50 u-text-5">
               {this.state.machinewin}
             </h2>
-            <h2 class="u-custom-font u-font-lobster u-text u-text-default u-text-grey-50 u-text-6">
+            <h2 className="u-custom-font u-font-lobster u-text u-text-default u-text-grey-50 u-text-6">
               {this.state.tie}
             </h2>
-            <h2 class="u-align-right-md u-align-right-sm u-align-right-xs u-custom-font u-font-lobster u-text u-text-default u-text-grey-50 u-text-7">
+            <h2 className="u-align-right-md u-align-right-sm u-align-right-xs u-custom-font u-font-lobster u-text u-text-default u-text-grey-50 u-text-7">
               {this.state.playerwin}
             </h2>
-            <div class="u-clearfix u-gutter-98 u-layout-wrap u-layout-wrap-1">
-              <div class="u-layout">
-                <div class="u-layout-row">
+            <div className="u-clearfix u-gutter-98 u-layout-wrap u-layout-wrap-1">
+              <div className="u-layout">
+                <div className="u-layout-row">
                   <div
-                    class={
+                    className={
                       "u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1 " +
                       this.state.shakeleft
                     }
@@ -231,12 +231,12 @@ export class Game extends Component {
                     data-image-height="1080"
                   >
                     <div
-                      class="u-container-layout u-valign-middle u-container-layout-1"
+                      className="u-container-layout u-valign-middle u-container-layout-1"
                       src=""
                     ></div>
                   </div>
                   <div
-                    class={
+                    className={
                       "u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-2 " +
                       this.state.shakeright
                     }
@@ -248,7 +248,7 @@ export class Game extends Component {
                     data-image-height="1080"
                   >
                     <div
-                      class="u-container-layout u-container-layout-2"
+                      className="u-container-layout u-container-layout-2"
                       src=""
                     ></div>
                   </div>
@@ -256,14 +256,14 @@ export class Game extends Component {
               </div>
             </div>
             <div
-              class="u-align-right u-container-style u-group u-image u-image-3"
+              className="u-align-right u-container-style u-group u-image u-image-3"
               style={{
                 backgroundImage: "url(" + loader + ")",
                 backgroundColor: "#2cccc4",
               }}
             >
               <Webcam
-                class="embed-responsive-item"
+                className="embed-responsive-item"
                 ref={this.webcamRef}
                 audio={false}
                 mirrored={true}
@@ -313,14 +313,14 @@ export class Game extends Component {
               >
                 {this.state.error}
               </div>
-              <div class="u-container-layout u-container-layout-3"></div>
+              <div className="u-container-layout u-container-layout-3"></div>
             </div>
-            <div class="u-container-style u-group u-shape-rectangle u-group-2">
-              <div class="u-container-layout u-container-layout-4">
-                <h1 class="u-align-center u-text u-text-palette-4-base u-text-8">
+            <div className="u-container-style u-group u-shape-rectangle u-group-2">
+              <div className="u-container-layout u-container-layout-4">
+                <h1 className="u-align-center u-text u-text-palette-4-base u-text-8">
                   How to play
                 </h1>
-                <p class="u-align-center u-text u-text-9">
+                <p className="u-align-center u-text u-text-9">
                   <span style={{fontWeight: "700"}}>1.</span> Keep your hand
                   within the camera frame.
                   <br />
